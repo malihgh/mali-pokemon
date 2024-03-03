b0ff6c44c8ca240e8f48b20a26fcfdf463e430ce
malihgh <malih.ghasemian@gmail.com>
2024-03-03 09:56:18 +0100

    feat : Add bounce animation in tailwind.config.ts.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,22 @@ const config: Config = {
       phone: { max: '740px' },
       tablet: { max: '1100px', min: '741px' }
     },
+    animation: {
+      bounce: 'bounce 0.3s 0.5'
+    },
+    keyframes: {
+      bounce: {
+        '0%, 100%': {
+          transform: 'translateY(-3%)',
+          'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1);'
+        },
+
+        '50%': {
+          transform: 'translateY(0)',
+          'animation-timing-function': 'cubic-bezier(0,0,0.2,1);'
+        }
+      }
+    },
     fontFamily: {
       flexo: ['flexo', 'sans-serif'],
       flexoBold: ['flexo-bold', 'sans-serif']
