93f16f16b166bfcfb06f5f3ddfe14f198cc16ec3
malihgh <malih.ghasemian@gmail.com>
2024-03-03 09:54:22 +0100

    feat : Create pokemonData and types.

---
+++ src/pages/Home/data/pokemonData.ts
@@ -0,0 +1,30 @@
+import { pokemonDataType } from '../types';
+
+export const pokemonData: pokemonDataType[] = [
+  {
+    id: 132,
+    name: 'ditto',
+    base_experience: 101,
+    // https://assets.pokemon.com/assets/cms2/img/pokedex/detail/132.png
+    sprite_image: {
+      //   front:
+      //     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png',
+      front:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/132.png',
+      back: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png'
+    },
+    abilities: [{ name: 'limber' }],
+    types: [{ name: 'normal' }, { name: 'superMali' }],
+    moves: [{ name: 'transform' }],
+    height: 3,
+    weight: 40,
+    stats: [
+      { base_stat: 48, name: 'hp' },
+      { base_stat: 48, name: 'attack' },
+      { base_stat: 48, name: 'defense' },
+      { base_stat: 48, name: 'special-attack' },
+      { base_stat: 48, name: 'special-defense' },
+      { base_stat: 48, name: 'speed' }
+    ]
+  }
+];
