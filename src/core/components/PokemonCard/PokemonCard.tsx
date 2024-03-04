d1db3094503e745b793413259d091eea9b0e8ee7
malihgh <malih.ghasemian@gmail.com>
2024-03-04 14:07:26 +0100

    feat : Use Link in PokemonCard to show the detail for each Card.

--- src/core/components/PokemonCard/PokemonCard.tsx
+++ src/core/components/PokemonCard/PokemonCard.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 import { FC } from 'react';
 import Text from '../Text';
 import DeckButton from '../DeckButton';
+import Link from 'next/link';
 
 type Props = {
   data: PokemonDataType;
@@ -20,54 +21,56 @@ const PokemonCard: FC<Props> = props => {
   const idFillWithZero = '#' + String(id).padStart(4, '0');
 
   return (
-    <div className="m-1 hover:animate-bounce">
-      {/* fix it later --> url */}
-      <div
-        className={classNames(
-          'w-full h-56 bg-no-repeat bg-cover bg-gray-200 rounded-lg',
-          `bg-[url('https://assets.pokemon.com/assets/cms2/img/pokedex/detail/132.png')]`
-        )}></div>
+    <Link href={`/info/${id}`}>
+      <div className="m-1 hover:animate-bounce">
+        {/* fix it later --> url */}
+        <div
+          className={classNames(
+            'w-full h-56 bg-no-repeat bg-cover bg-gray-200 rounded-lg',
+            `bg-[url('https://assets.pokemon.com/assets/cms2/img/pokedex/detail/132.png')]`
+          )}></div>
 
-      <div className="pb-5 px-3">
-        <Text type="s" className="text-sm font-flexoBold text-[#919191]">
-          {idFillWithZero}
-        </Text>
+        <div className="pb-5 px-3">
+          <Text type="s" className="text-sm font-flexoBold text-[#919191]">
+            {idFillWithZero}
+          </Text>
 
-        <Text type="h3" className="text-[#000] font-flexoBold mt-3 mb-2">
-          {capitalizeName(name)}
-        </Text>
+          <Text type="h3" className="text-[#000] font-flexoBold mt-3 mb-2">
+            {capitalizeName(name)}
+          </Text>
 
-        {isSearched && (
-          <>
-            <Text type="s" className="mb-1">
-              Base experience: {base_experience}
-            </Text>
-
-            {/* fix it later -> , between abilities */}
-            <Text type="s" className="mb-2">
-              Abilities:{' '}
-              {abilities.map((ability, index) => (
-                <>{capitalizeName(ability.name)}</>
-              ))}
-            </Text>
-          </>
-        )}
-
-        <div className="flex justify-start mb-3">
-          {types.map((type, index) => (
-            <div
-              key={index}
-              className="px-4 py-0.5 rounded-md bg-gray-300 mr-2">
-              <Text type="s" className="text-xs">
-                {capitalizeName(type.name)}
+          {isSearched && (
+            <>
+              <Text type="s" className="mb-1">
+                Base experience: {base_experience}
               </Text>
-            </div>
-          ))}
-        </div>
 
-        {isSearched && <DeckButton pokemonId={id} />}
+              {/* fix it later -> , between abilities */}
+              <Text type="s" className="mb-2">
+                Abilities:{' '}
+                {abilities.map((ability, index) => (
+                  <>{capitalizeName(ability.name)}</>
+                ))}
+              </Text>
+            </>
+          )}
+
+          <div className="flex justify-start mb-3">
+            {types.map((type, index) => (
+              <div
+                key={index}
+                className="px-4 py-0.5 rounded-md bg-gray-300 mr-2">
+                <Text type="s" className="text-xs">
+                  {capitalizeName(type.name)}
+                </Text>
+              </div>
+            ))}
+          </div>
+
+          {isSearched && <DeckButton pokemonId={id} />}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
