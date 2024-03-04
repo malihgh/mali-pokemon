fee6d7cbc339eff8c8cbe885b76d92ab93dd99f6
malihgh <malih.ghasemian@gmail.com>
2024-03-04 12:34:57 +0100

    feat : Clean code for Info page.

--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -1,11 +1,10 @@
 import Container from '@/core/components/Container';
-import Text from '@/core/components/Text';
 import { PokemonDataType } from '../Home/types';
 import { FC } from 'react';
-import Image from 'next/image';
-import ChartContainer from './components/ChartContainer';
 import { pokemonData } from '../Home/data/pokemonData';
-import DeckButton from '@/core/components/DeckButton';
+import InfoHeader from './components/InfoHeader';
+import InfoDetails from './components/InfoDetails';
+import InfoImages from './components/InfoImages';
 
 type Props = {
   data: PokemonDataType;
@@ -18,93 +17,26 @@ const Info: FC<Props> = props => {
   const capitalizeName = (name: string) =>
     name.charAt(0).toUpperCase() + name.slice(1);
 
+  console.log('pppp');
   const idFillWithZero = '#' + String(id).padStart(4, '0');
   return (
     <Container className="px-10">
-      <div className="w-full flex phone:flex-col items-center justify-around phone:justify-between">
-        <div className="flex items-center phone:mb-5">
-          <Text type="h2" className="text-black font-flexo font-normal mr-4">
-            {capitalizeName(name)}
-          </Text>
-          <Text type="h2" className="text-gray-700">
-            {idFillWithZero}
-          </Text>
-        </div>
-
-        <DeckButton pokemonId={id} />
-      </div>
+      <InfoHeader
+        name={capitalizeName(name)}
+        id={id}
+        idWithZero={idFillWithZero}
+      />
 
       <div className="w-full flex phone:flex-col items-start mt-12 phone:mt-5">
-        <div className="flex-grow max-w-[50%] phone:max-w-full phone:w-full mr-6 phone:mr-0">
-          <Image
-            src={sprite_image.front}
-            alt={'pic'}
-            width={430}
-            height={400}
-            className="bg-gray-200 rounded-md mb-6"
-          />
+        <InfoImages image={sprite_image.front} stats={pokemonData[0].stats} />
 
-          <ChartContainer stats={pokemonData[0].stats} />
-        </div>
-
-        <div className="flex-grow mt-10 phone:w-full phone:mt-5">
-          <div className="bg-[#30a7d7] p-7 pr-20 phone:p-5 rounded-md phone:mb-5">
-            <div className="flex mb-2 justify-between">
-              <div>
-                <Text type="s" className="text-white mb-2">
-                  Height
-                </Text>
-                <Text type="s" className="text-black mb-2">
-                  {height} cm
-                </Text>
-              </div>
-
-              <div>
-                <Text type="s" className="text-white mb-2">
-                  Weight
-                </Text>
-                <Text type="s" className="text-black mb-2">
-                  {weight} kg
-                </Text>
-              </div>
-            </div>
-
-            <Text type="s" className="text-white mb-2">
-              Abilities:{' '}
-              {abilities.map((ability, index) => (
-                <Text type="s" className="text-black" key={index}>
-                  {capitalizeName(ability.name)}
-                </Text>
-              ))}
-            </Text>
-          </div>
-
-          <Text type="s" className="my-4">
-            Type
-          </Text>
-          <div className="flex justify-start">
-            {types.map((type, index) => (
-              <div
-                key={index}
-                className="px-4 py-0.5 rounded-md bg-gray-300 mr-2">
-                <Text type="s">{capitalizeName(type.name)}</Text>
-              </div>
-            ))}
-          </div>
-
-          <Text type="s" className="mt-7 mb-4">
-            Moves
-          </Text>
-          <div className="flex justify-start">
-            {moves.map((type, index) => (
-              <div
-                key={index}
-                className="px-4 py-0.5 rounded-md bg-gray-300 mr-2">
-                <Text type="s">{capitalizeName(type.name)}</Text>
-              </div>
-            ))}
-          </div>
-        </div>
+        <InfoDetails
+          height={height}
+          weight={weight}
+          abilities={abilities.map(i => capitalizeName(i.name))}
+          types={types.map(i => capitalizeName(i.name))}
+          moves={moves.map(i => capitalizeName(i.name))}
+        />
       </div>
     </Container>
   );
---
+++ src/pages/Info/components/InfoHeader/InfoHeader.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import Text from '@/core/components/Text';
+import DeckButton from '@/core/components/DeckButton';
+
+type Props = {
+  name: string;
+  id: number;
+  idWithZero: string;
+};
+
+const InfoHeader: FC<Props> = ({ name, id, idWithZero }) => {
+  return (
+    <div className="w-full flex phone:flex-col items-center justify-around phone:justify-between">
+      <div className="flex items-center phone:mb-5">
+        <Text type="h2" className="text-black font-flexo font-normal mr-4">
+          {name}
+        </Text>
+        <Text type="h2" className="text-gray-700">
+          {idWithZero}
+        </Text>
+      </div>
+
+      <DeckButton pokemonId={id} />
+    </div>
+  );
+};
+
+export default InfoHeader;
