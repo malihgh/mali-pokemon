4b2f8255e0031927fc2ac0bf182847ff0ee2ad87
malihgh <malih.ghasemian@gmail.com>
2024-03-04 09:20:49 +0100

    feat : Add Info in main page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Home from '@/pages/Home';
+import { pokemonData } from '@/pages/Home/data/pokemonData';
+import Info from '@/pages/Info';
 
 export default function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <Info data={pokemonData[0]} />
     </>
   );
 }
