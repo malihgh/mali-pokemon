4067e14becfe561f56c25b7406233bc3245da116
malihgh <malih.ghasemian@gmail.com>
2024-03-06 12:32:17 +0100

    🔥 feat : Create DeckListContext to handling deckList.

--- src/core/providers/ContextProvider.tsx
+++ src/core/providers/ContextProvider.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import PokemonListContext from '../context/pokemonListContext';
 import { PokemonDataType } from '@/pages/Home/types';
 import { pokemonDataDefault } from '@/pages/Home/data/pokemonDataDefault';
+import DeckListContext from '../context/deckListContext';
 
 export default function ContextProvider({
   children
@@ -12,9 +13,13 @@ export default function ContextProvider({
 }) {
   const [pokemonData, setPokemonData] =
     useState<PokemonDataType[]>(pokemonDataDefault);
+  const [deckList, setDeckList] = useState<number[]>([]);
+
   return (
     <PokemonListContext.Provider value={{ pokemonData, setPokemonData }}>
-      {children}
+      <DeckListContext.Provider value={{ list: deckList, setDeckList }}>
+        {children}
+      </DeckListContext.Provider>
     </PokemonListContext.Provider>
   );
 }
